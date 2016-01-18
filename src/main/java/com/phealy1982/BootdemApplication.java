889406e329504da1e3162f1d279f050c0b5cade4
Peter Healy <phealy1982@gmail.com>
2016-01-18 18:51:18 +0000

    initial spring boot

---
+++ src/main/java/com/phealy1982/BootdemApplication.java
@@ -0,0 +1,12 @@
+package com.phealy1982;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootdemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootdemApplication.class, args);
+	}
+}
---
+++ src/test/java/com/phealy1982/BootdemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.phealy1982;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = BootdemApplication.class)
+public class BootdemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
