6615c91cab60429b31a8430d1dccda1f4c11d141
Peter Healy <phealy1982@gmail.com>
2016-01-18 23:48:54 +0000

    restful additions

--- src/main/java/com/phealy1982/BootdemApplication.java
+++ src/main/java/com/phealy1982/BootdemApplication.java
@@ -1,12 +1,22 @@
 package com.phealy1982;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 public class BootdemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootdemApplication.class, args);
+		
+		System.out.println("starting app...");	
 	}
 }
+
---
+++ src/main/java/com/phealy1982/InfoController.java
@@ -0,0 +1,12 @@
+package com.phealy1982;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InfoController {
+	@RequestMapping("/info")
+    String info() {
+        return "App is up and running";
+    }
+}
