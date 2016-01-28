a2cdf03c55e446267b49caa1df3efce4619af916
Peter Healy <phealy1982@gmail.com>
2016-01-28 23:08:34 +0000

    yo

--- src/main/java/com/phealy1982/InfoController.java
+++ src/main/java/com/phealy1982/InfoController.java
@@ -1,5 +1,6 @@
 package com.phealy1982;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,13 +8,13 @@ import com.phealy1982.models.Stuff;
 
 @RestController
 public class InfoController {
-	@RequestMapping("/info")
-    String info() {
+	@RequestMapping("/info/{number}")
+    String info(@PathVariable(value="number") String id) {
 
 		
 		Stuff s = new Stuff();
 		
-		s.setNameOfStuff("le stuff");
+		s.setNameOfStuff(id);
 		
         return "App is up and running: " + s.getNameOfStuff();
 
