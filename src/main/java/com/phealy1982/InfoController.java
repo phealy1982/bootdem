6ba080953db6e687f23a5d6a99eda2ee2ee77f96
Peter Healy <phealy1982@gmail.com>
2016-01-28 23:02:44 +0000

    lombokin

--- src/main/java/com/phealy1982/InfoController.java
+++ src/main/java/com/phealy1982/InfoController.java
@@ -3,10 +3,17 @@ package com.phealy1982;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.phealy1982.models.Stuff;
+
 @RestController
 public class InfoController {
 	@RequestMapping("/info")
     String info() {
-        return "App is up and running";
+		
+		Stuff s = new Stuff();
+		
+		s.setNameOfStuff("le stuff");
+		
+        return "App is up and running: " + s.getNameOfStuff();
     }
 }
---
+++ src/main/java/com/phealy1982/models/Stuff.java
@@ -0,0 +1,20 @@
+package com.phealy1982.models;
+
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Stuff {
+
+	private String nameOfStuff;
+	private String sizeOfStuff;
+	
+	
+	public void setNameOfStuff(String yo,int c){
+		
+	}
+}
